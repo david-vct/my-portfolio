64e76aa96da311bf18374efb7a28db1bf4b94bb6
David Vicente <davidvicento@gmail.com>
2024-09-25 10:52:12 +0200

    feat: add projects section

--- app/components/common/Project.tsx
+++ app/components/common/Project.tsx
@@ -1,25 +1,39 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Image from "next/image"
 import React from "react"
+import ButtonLink from "./ButtonLink"
+import { faSquareArrowUpRight } from "@fortawesome/free-solid-svg-icons"
 
 type Props = {
 	title: string
 	description: string
 	images: string[]
+	site?: string
+	github?: string
 }
 
-const Project = ({ title, description, images }: Props) => {
+const Project = ({ title, description, images, site, github }: Props) => {
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-8">
-			<div className="">
+		<div className="grid grid-cols-1 lg:grid-cols-2 place-items-center gap-8 p-8">
+			<div className="flex flex-row flex-nowrap w-full h-full overflow-x-auto">
 				{images.map((image, index) => (
-					<div className="relative w-full min-h-80" key={index}>
+					<div className="relative flex-none w-full min-h-80" key={index}>
 						<Image className="object-contain" alt={image} src={image} key={index} fill={true} />
 					</div>
 				))}
 			</div>
 			<div>
-				<h2 className="text-4xl font-bold">{title}</h2>
-				<p className="text-xl">{description}</p>
+				<h2 className="pb-8 text-4xl font-bold">{title}</h2>
+				<p className="pb-8 text-xl">{description}</p>
+				<div className="flex flex-row items-center gap-8">
+					{site && <ButtonLink text="Site" link={site} icon={faSquareArrowUpRight}></ButtonLink>}
+					{github && (
+						<a href={github} target="_blank">
+							<FontAwesomeIcon className="h-8" icon={faGithub} />
+						</a>
+					)}
+				</div>
 			</div>
 		</div>
 	)
--- app/components/layout/ProjectSection.tsx
+++ app/components/layout/ProjectSection.tsx
@@ -3,15 +3,21 @@ import Project from "../common/Project"
 
 const ProjectSection = () => {
 	return (
-		<section className="w-full md:w-2/3 p-16 bg-primary/20">
-			<h1 className="text-4xl font-bold pb-16">Mes Projets</h1>
+		<section className="w-full md:w-2/3 p-16">
 			<div className="flex flex-col">
 				<Project
 					title="Vocabulary Tools pour Google Docs"
 					description="Extension pour Google Docs qui fournit un accès instantané aux synonymes, antonymes et conjugaisons"
 					images={["/project-vocabularytools-1.jpg"]}
+					github={"https://github.com/david-vct/gdocs-vocabulary-tools"}
 				></Project>
-				<div>Project 2</div>
+				<Project
+					title="Rculture"
+					description="Jeu multijoueur captivant où les participants répondent à des quiz créés par la communauté. Défie tes amis et découvre de nouvelles questions à chaque partie !"
+					images={["/project-rculture-1.png", "/project-rculture-2.png", "/project-rculture-3.png"]}
+					site={"https://rculture.web.app"}
+					github={"https://github.com/david-vct/rculture"}
+				></Project>
 			</div>
 		</section>
 	)
